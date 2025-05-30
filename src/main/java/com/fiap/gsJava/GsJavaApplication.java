d34a42e93fb5003ba3794d2619999dc6a8ed843f
wanderluzter <leobotelho27@outlook.com>
2025-05-30 08:39:28 -0300

    Projeto criado.

---
+++ src/main/java/com/fiap/gsJava/GsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.gsJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/gsJava/GsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.gsJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
